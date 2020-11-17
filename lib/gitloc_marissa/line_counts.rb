7d8d40fa56ec5b91c7ba3d70b380ad44f0531690
Marissa <marissa.biesecker@gmail.com>
2020-11-17 19:00:03 +0000

    Change line count class to module

--- lib/gitloc_marissa/line_counts.rb
+++ lib/gitloc_marissa/line_counts.rb
@@ -1,5 +1,5 @@
 class GitlocMarissa
-  class LineCounts
+  module LineCounts
     def self.call(files_and_bodies)
       files_and_counts = files_and_bodies.map do |file, body|
         count = body.lines.count { |line| line !~ /^\s*$/ }
