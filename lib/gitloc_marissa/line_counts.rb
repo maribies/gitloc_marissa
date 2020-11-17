aa1b903ad4a21c62edbc5d0701fc279c3ac4e8f9
Marissa <marissa.biesecker@gmail.com>
2020-11-17 18:57:19 +0000

    Omit binary files from counts

--- lib/gitloc_marissa/line_counts.rb
+++ lib/gitloc_marissa/line_counts.rb
@@ -1,10 +1,12 @@
 class GitlocMarissa
   class LineCounts
     def self.call(files_and_bodies)
-      files_and_bodies.map do |file, body|
+      files_and_counts = files_and_bodies.map do |file, body|
         count = body.lines.count { |line| line !~ /^\s*$/ }
         [file, count]
+      rescue ArgumentError
       end
+      files_and_counts.compact
     end
   end
 end
