945136d919d024d57f6e4a41f7234d8f083c2fda
Marissa <marissa.biesecker@gmail.com>
2020-11-17 18:29:37 +0000

    Exract line count logic to new class

--- lib/gitloc_marissa.rb
+++ lib/gitloc_marissa.rb
@@ -4,8 +4,18 @@ require 'gitloc_marissa/version'
 require 'gitloc_marissa/errors'
 
 class GitlocMarissa
+  class LineCounts
+    def self.call(files_and_bodies)
+      files_and_bodies.map do |file, body|
+        count = body.lines.count { |line| line !~ /^\s*$/ }
+        [file, count]
+      end
+    end
+  end
+
+
   def self.call(repo)
-    Dir.mktmpdir do |dir|
+    files_and_bodies = Dir.mktmpdir do |dir|
       Dir.chdir dir do
         out, err, status = Open3.capture3 'git', 'clone', repo, 'cloned'
 
@@ -14,10 +24,11 @@ class GitlocMarissa
         Dir.chdir 'cloned'
         files = Dir['**/*'].reject { |name| File.directory? name }
         files.map do |file|
-          count = File.readlines(file).count { |line| line !~ /^\s*$/ }
-          [file, count]
+          body = File.read(file)
+          [file, body]
         end
       end
     end
+    LineCounts.call(files_and_bodies)
   end
 end
